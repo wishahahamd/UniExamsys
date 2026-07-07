364e1b49883d944b25af40689e779c2efa595667
wishahahamd <ahmadvisha86@gmail.com>
2026-07-07 06:00:28 +0500

    Upload includes/footer.php via Examsys CLI Uploader

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<?php
+// includes/footer.php
+?>
+            </div> <!-- End content-wrapper -->
+        </div> <!-- End main-content -->
+    </div> <!-- End dashboard-container -->
+</body>
+</html>
